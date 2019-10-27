1347fb88ccd96fad5e14a78a9e4c256454aa4482
Mailani Gelles <mgelles@usc.edu>
2019-10-26 21:31:52 -0700

    Update ultrasonicServer.py

--- TCP,UDP, and Port Forwarding/part3/ultrasonicServer.py
+++ TCP,UDP, and Port Forwarding/part3/ultrasonicServer.py
@@ -4,17 +4,15 @@
 # sensor data and prints it to stdout. Use a UDP socket here.
 # 
 # Team members: Emily Kuo, Mailani Gelles
-# Github repository link:
-# https://github.com/usc-ee250-fall2019/lab03-emily-mailani.git
 
 import socket
 
 def Main():
     # Change the host and port as needed. For ports, use a number in the 10000s
     # range. 
-    #emily: host = '172.16.10.137' #VM's IP address
-    #mailani:
-    host = '192.168.0.239'
+    #emily: 
+    host = '172.16.10.137' #VM's IP address
+    #mailani: host = '192.168.0.239'
     port = 9000
 
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
